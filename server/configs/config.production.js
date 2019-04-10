8db4b48b2abfc19ba015ebdb7a3337c40e55bf1a
asribeiro.rafael <asribeiro.rafael@gmail.com>
2019-04-10 14:17:14 -0300

    Template NodeJS BackEnd 1.0

--- server/configs/config.production.js
+++ server/configs/config.production.js
@@ -4,6 +4,5 @@ module.exports = {
     },
     DB: {
         url: 'ADICIONAR BASE DE DADOS MONGODB'
-        //url: 'mongodb://admin_master:osys2018@ds161529.mlab.com:61529/osys_db_teste'
     }
 };
